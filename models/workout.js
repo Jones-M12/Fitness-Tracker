904ce8c133a8aec0cd2eaae0325593f6ede8d54b
Jones-M <61522666+Jones-M12@users.noreply.github.com>
2020-07-08 14:35:13 -0400

    setup models

--- models/workout.js
+++ models/workout.js
@@ -1,2 +1,42 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  type: {
+    type: String,
+    trim: true,
+    required: "Select or enter a workout type."
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "Select or enter a name for the workout."
+  },
+  duration: {
+    type: Number,
+    
+  },
+  weight: {
+    type: Number,
+    
+  },
+  reps: {
+    type: Number,
+    
+  },
+  sets: {
+    type: Number,
+    
+  },
+  distance: {
+    type: Number,
+    
+  },
+  
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
