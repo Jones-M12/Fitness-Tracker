e1101cdb65072953361271b5a9e2ce3adaa74c36
Jones-M <61522666+Jones-M12@users.noreply.github.com>
2020-07-10 17:51:21 -0400

    setup api-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const Transaction = require("../models/workout.js");
 
-//Setup routes to Handlebar Pages
+//Setup routes to 
 module.exports = function(app){
 
+    app.get('/', (req, res) => {
+        res.send('exercise');
+      });
     app.get('/exercise', (req, res) => {
         res.send('exercise');
       });
