ac1b7b9f6439cb6b29c604fab1ec42bbccaec490
Jones-M <61522666+Jones-M12@users.noreply.github.com>
2020-07-10 11:38:56 -0400

    update models folder files

--- models/workout.js
+++ models/workout.js
@@ -3,37 +3,46 @@ const Schema = mongoose.Schema;
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  type: {
-    type: String,
-    trim: true,
-    required: "Select or enter a workout type."
-  },
-  name: {
-    type: String,
-    trim: true,
-    required: "Select or enter a name for the workout."
-  },
-  duration: {
-    type: Number,
-    
-  },
-  weight: {
-    type: Number,
-    
-  },
-  reps: {
-    type: Number,
-    
-  },
-  sets: {
-    type: Number,
-    
-  },
-  distance: {
-    type: Number,
-    
-  },
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        },
+        session: [{
+
+            type: {
+                type: String,
+                trim: true,
+                required: "Select or enter a workout type."
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Select or enter a name for the workout."
+            },
+            duration: {
+                type: Number,
+                
+            },
+            weight: {
+                type: Number,
+                
+            },
+            reps: {
+                type: Number,
+                
+            },
+            sets: {
+                type: Number,
+                
+            },
+            distance: {
+                type: Number,
+                
+            },
+    }]
+
   
 });
 
